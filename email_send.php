798358fd319e2bc8d488061e64ee4c69173c3079
Dairon Rodriguez <drodriguez@fvi.edu>
2017-09-27 21:48:35 -0400

    correct typos in diferents sections

--- email_send.php
+++ email_send.php
@@ -1,23 +1,23 @@
 <?php
-if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $msg=$_POST['msg'];
+// if(isset($_POST['submit'])){
+//
+// }
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$msg=$_POST['msg'];
 
 
-    $to='dairolrpichardo90@yahoo.com';
-    $subject='Website Message';
-    $message="Name: ".$name."\n"."Wrote the following: "."\n\n".$msg;
-    $headers="From: ".$email;
+$to='dairolrpichardo90@gmail.com';
+$subject='Website Message';
+$message="Name: ".$name."\n"."Wrote the following: "."\n\n".$msg;
+$headers="From: ".$email;
 
-
-    if(mail($to, $subject, $message, $headers)) {
-        echo "<h1>Sent Successfully! Thank you"." ".$name.",Im appreciate your interest!</h1>";
-    }
-    else{
-        echo "Something went wrong!";
-    }
-
+if(mail($to, $subject, $message, $headers)) {
+    echo "<h1>Sent Successfully! Thank you"." ".$name.",I appreciate your interest!</h1>";
+}
+else{
+    echo "Something went wrong!";
 }
 
 ?>
---
+++ js/contact.js
@@ -0,0 +1,11 @@
+// contactForm.addEventListener('submit', function(event) {
+//     event.preventDefault()
+//
+//     $.ajax({
+//         url: '/email_send.php',
+//         method: 'POST'
+//     })
+//     .done(function(response) {
+//         console.log(response);
+//     })
+// })
